962524ece5d60eb845e4f227e1cfc52e824dd35a
gabelorenzo <gtlorenzo93@gmail.com>
2021-08-02 00:03:17 -0400

    Configure staging environment for Heroku

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:4000/api',
-  // apiUrl: 'https://ratemystocks.com/api',
-  // TODO: set this back to the production URL after done testing Heroku deployment
-  apiUrl: 'https://ratemystocks-staging.herokuapp.com/api',
+  apiUrl: 'https://ratemystocks.com/api',
 };
