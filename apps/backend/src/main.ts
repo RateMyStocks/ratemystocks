c7477bee7b6a8e1914506ad818828cae07678d7a
gabelorenzo <gtlorenzo93@gmail.com>
2021-09-14 01:44:52 -0400

    Another attempt at forcing SSL for Heroku

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -14,6 +14,7 @@ import sslRedirect from 'heroku-ssl-redirect';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.use(sslRedirect());
 
   app.setGlobalPrefix('api');
 
@@ -26,8 +27,6 @@ async function bootstrap() {
       methods: ['GET, POST, PATCH, DELETE, PUT, OPTIONS'],
     });
   } else {
-    app.use(sslRedirect());
-
     const whitelist = ['https://ratemystocks.com', 'https://ratemystocks-staging.herokuapp.com'];
     app.enableCors({
       credentials: true,
