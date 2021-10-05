80fb6ba6d01b281611a0009359893779fa952878
gabelorenzo <gtlorenzo93@gmail.com>
2021-10-04 23:46:38 -0400

    Configure app to connect to Redis on Heroku

--- apps/backend/src/redis.ts
+++ apps/backend/src/redis.ts
@@ -1,3 +1,6 @@
 import * as Redis from 'ioredis';
 
-export const redis = new Redis();
+require('dotenv').config();
+
+// You must specify NODE_ENV due to a weird NestJS/Nx issue: https://stackoverflow.com/questions/58090082/process-env-node-env-always-development-when-building-nestjs-app-with-nrwl-nx
+export const redis = process.env['NODE' + '_ENV'] === 'local' ? new Redis() : new Redis(process.env.REDIS_URL);
