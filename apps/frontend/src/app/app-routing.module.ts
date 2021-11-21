65c5c423b08ad0e910e73af8fc5aec3ce1a8ef45
gabelorenzo <gtlorenzo93@gmail.com>
2021-11-21 16:39:06 -0500

    Fix production routing since the Coming Soon page is removed

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, Route } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { RESOURCES_ROUTES } from './modules/resources/resources.routes';
 import { NotFoundComponent } from './modules/error/not-found/not-found.component';
 
-import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
-
-// TODO: Until official launch, we are just going to show a "Coming Soon" page on production (so no need for redirect on production).
-const homeRoute: Route = environment.production
-  ? { path: '', component: AppComponent }
-  : {
-      path: '',
-      redirectTo: '/stocks',
-      pathMatch: 'full',
-    };
-
 const routes: Routes = [
-  homeRoute,
+  {
+    path: '',
+    redirectTo: '/stocks',
+    pathMatch: 'full',
+  },
   {
     path: 'stocks',
     loadChildren: () => import('./modules/stock/stock.module').then((m) => m.StockModule),
