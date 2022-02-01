429bbb21df6fb5099b098ab24f79e3622496cd27
gabelorenzo <gtlorenzo93@gmail.com>
2022-01-31 19:35:04 -0500

    Fix CSS styles not loading with Angular Universal in Heroku due to not compiling Sass before running ng build

--- heroku-build.sh
+++ heroku-build.sh
@@ -2,13 +2,13 @@
 
 echo "Building Angular app for $NODE_ENV"
 
-build_staging='ng build --configuration staging && ng run frontend:server:staging'
+build_staging='sass --update apps/frontend/src/assets:apps/frontend/src/assets --no-source-map && ng build --configuration staging && ng run frontend:server:staging'
 if [[ $NODE_ENV = "staging" ]]; then
  echo "running $build_staging ..."
  eval "$build_staging"
 fi
 
-build_prod='ng build --prod && ng run frontend:server:production'
+build_prod='sass --update apps/frontend/src/assets:apps/frontend/src/assets --no-source-map && ng build --prod && ng run frontend:server:production'
 if [[ $NODE_ENV = "production" ]]; then
  echo "running $build_prod ..."
  eval "$build_prod"
