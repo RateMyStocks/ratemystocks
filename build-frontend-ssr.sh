f5b4116a92984b8ded8aafebf17c9437d92039fb
Gabe Lorenzo <gtlorenzo93@gmail.com>
2023-08-20 22:44:08 -0400

    More potential fixes for Render deployment

--- build-frontend-ssr.sh
+++ build-frontend-ssr.sh
@@ -3,12 +3,15 @@
 echo "Building Angular app for $NODE_ENV"
 
 build_staging='sass --update apps/frontend/src/assets:apps/frontend/src/assets --no-source-map && ng build --configuration staging && ng run frontend:server:staging'
+echo $build_staging
 if [[ $NODE_ENV = "staging" ]]; then
  echo "running $build_staging ..."
  eval "$build_staging"
 fi
 
-build_prod='sass --update apps/frontend/src/assets:apps/frontend/src/assets --no-source-map && ng build --prod && ng run frontend:server:production'
+# build_prod='sass --update apps/frontend/src/assets:apps/frontend/src/assets --no-source-map && ng build --prod && ng run frontend:server:production'
+build_prod='npm run build:ssr:helper:prod'
+echo $build_prod
 if [[ $NODE_ENV = "production" ]]; then
  echo "running $build_prod ..."
  eval "$build_prod"
